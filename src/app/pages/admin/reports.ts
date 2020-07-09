5bc4eb81a7e7847985c64921cf70f0a7dc231d63
mark <markaris.trinidad@gmail.com>
2020-07-09 14:45:00 +0800

    jsreport download

--- src/app/pages/admin/reports.ts
+++ src/app/pages/admin/reports.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core'
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams,  } from '@angular/common/http';
 
 @Component({
     styles: [`
@@ -50,17 +50,65 @@ export class ReportsAdmin implements OnInit, OnDestroy {
  
 
         // this.http.post('/api/clientreport/staff-listing', data, { responseType: 'blob', reportProgress: true }).subscribe(blob => {
-        //     let data = window.URL.createObjectURL(blob);
-        //     let link = document.createElement('a');
-        //     link.href = data;
-        //     link.download = 'filename.pdf';
-        //     link.click();
+            // let data = window.URL.createObjectURL(blob);
+            // let link = document.createElement('a');
+            // link.href = data;
+            // link.download = 'filename.pdf';
+            // link.click();
 
-        //     setTimeout(() => {
-        //         window.URL.revokeObjectURL(data);
-        //     }, 100);
+            // setTimeout(() => {
+            //     window.URL.revokeObjectURL(data);
+            // }, 100);
         // }, err => {
           
         // })
+    }
+
+    goJSReport(){
+        const data =    {
+            "template": {  "_id": "Ytn4HC6Slzi8OyWI"  },
+            "data" : {
+                "number": "123",
+                "seller": {
+                    "name": "Next Step Webs, Inc.",
+                    "road": "12345 Sunny Road",
+                    "country": "Sunnyville, TX 12345"
+                },
+                "buyer": {
+                    "name": "Acme Corp.",
+                    "road": "16 Johnson Road",
+                    "country": "Paris, France 8060"
+                },
+                "items": [{
+                    "name": "Website design",
+                    "price": 300
+                }]
+            },
+            "options": { "reports": { "save": true } }
+         }
+ 
+        const headerDict = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            // 'Access-Control-Allow-Headers': 'Content-Type',
+          }
+          
+        const requestOptions = {
+            headers: new HttpHeaders(headerDict)
+        };
+
+        this.http.post('http://localhost:5488/api/report', JSON.stringify(data), { headers: requestOptions.headers, responseType: 'blob' }).subscribe(blob => {
+            let data = window.URL.createObjectURL(blob);
+            let link = document.createElement('a');
+            link.href = data;
+            link.download = 'filename.pdf';
+            link.click();
+
+            setTimeout(() => {
+                window.URL.revokeObjectURL(data);
+            }, 100);
+        }, err => {
+            console.log('err')
+        });
     }
 }

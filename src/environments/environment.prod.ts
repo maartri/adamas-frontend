4bb27fccf472110fa72565855cbfdef2c23da900
mark <markaris.trinidad@gmail.com>
2020-08-10 14:55:01 +0800

    cccc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: false,
   versionCheckURL : 'https://rdweb.sjccl.org.au/portal/version.json'
 };
-// url http://localhost/ https://rdweb.sjccl.org.au/portal/ 
+// url http://localhost/ https://rdweb.sjccl.org.au/portal/

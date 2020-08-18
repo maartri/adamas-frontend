c7def0cb4933516a082097e589d85798b3f87fcc
arshadblouch81 <arshadblouch81@gmail.com>
2020-08-18 18:44:37 +1000

    arshad changes

--- src/app/pages/admin/rosters.ts
+++ src/app/pages/admin/rosters.ts
@@ -3,10 +3,13 @@ import { FullCalendarComponent, CalendarOptions } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-
-import { Subject } from 'rxjs';
+import { forkJoin,  Subject ,  Observable } from 'rxjs';
 import {debounceTime, distinctUntilChanged, takeUntil} from 'rxjs/operators';
 import { StaffService, GlobalService } from '@services/index';
+
+
+
+
 
 import startOfMonth from 'date-fns/startOfMonth';
 import endOfMonth from 'date-fns/endOfMonth';
@@ -29,6 +32,54 @@ import * as $ from 'jquery';
             float: left;
             margin: 1rem 0 0 0;
         }
+   
+    .dm-input{
+        margin-bottom:1rem;
+    }
+    nz-modal.options >>> div div div.ant-modal div.ant-modal-content div.ant-modal-body{
+        padding:0;
+    }
+    nz-modal.options >>> div div div.ant-modal div.ant-modal-content div.ant-modal-footer{
+        padding:0;
+    }
+    ul{
+        list-style: none;
+        padding: 5px 0 5px 15px;
+        margin: 0;
+    }
+    li {
+        padding: 4px 0 4px 10px;
+        font-size: 13px;
+        position:relative;
+        cursor:pointer;
+    }
+    li:hover{
+        background:#f2f2f2;
+    }
+    li i {
+        float:right;
+        margin-right:7px;
+    }
+    hr{
+        border: none;
+        height: 1px;
+        background: #e5e5e5;
+        margin: 2px 0;
+    }
+    li > ul{
+        position: absolute;
+        display:none;         
+        right: -192px;
+        padding: 2px 5px;
+        background: #fff;
+        top: -6px;
+        width: 192px;
+        transition: all 0.5s ease 3s;
+    }
+    li:hover > ul{           
+        display:block;
+        transition: all 0.5s ease 0s;
+    }
     `],
     templateUrl: './rosters.html'
 })
@@ -43,6 +94,13 @@ export class RostersAdmin implements OnInit, OnDestroy, AfterViewInit {
 
     isVisible: boolean = false;
     hahays = new Subject<any>();
+    optionsModal:boolean=false;
+    recipientDetailsModal:boolean=false;
+    changeModalView = new Subject<number>();
+    changeViewRecipientDetails = new Subject<number>();
+    _highlighted: Array<any> = [];
+    user:any;
+    selectedOption:any;
 
     calendarOptions: CalendarOptions = {
         initialView: 'timeGridMonth',
@@ -59,6 +117,8 @@ export class RostersAdmin implements OnInit, OnDestroy, AfterViewInit {
         // eventClassNames: [ 'myclassname', 'otherclassname' ],
         // eventContent: { html: '<i>some html</i>' },
         // eventDidMount: this.rightClick.bind(this),
+
+
         headerToolbar: {
             left: '',
             center: '',
@@ -86,12 +146,23 @@ export class RostersAdmin implements OnInit, OnDestroy, AfterViewInit {
         }        
     };
 
+    
+
+        
+
+
     onRightClick(e){
-        console.log(e.target);
+     
+        this.optionsModal=true;
+
+        console.log(this.optionsModal);
+
     }
 
     handleClick(e){
-        console.log(e);
+       // console.log(e);
+        this.optionsModal=true;
+        console.log(this.optionsModal);
     }
 
     calendarPlugins = [dayGridPlugin,timeGridPlugin,interactionPlugin]; // important!
@@ -146,6 +217,23 @@ export class RostersAdmin implements OnInit, OnDestroy, AfterViewInit {
                 this.loading = true;                         
                 this.searchRoster(this.date);          
             });
+
+            this.changeViewRecipientDetails.subscribe(data => {
+                // console.log(data);
+                this.user = {
+                  //  name: this.selectedOption.Recipient,
+                  //  code: this.selectedOption.uniqueid,
+                    startDate: '2019/01/15',
+                    endDate: '2019/01/29'
+                }            
+                // this.tabvrd = data;
+                this.recipientDetailsModal = true;
+            });
+    
+            this.changeModalView.subscribe(data => {
+                console.log(data);
+            });
+        
     }
 
     ngOnInit(): void {
@@ -154,6 +242,25 @@ export class RostersAdmin implements OnInit, OnDestroy, AfterViewInit {
 
     ngOnDestroy(): void {
 
+    }
+    showOptions(data: any) {
+        console.log(data);
+        this.selectedOption = data.selected; 
+
+        var uniqueIds = this._highlighted.reduce((acc, data) => {
+            acc.push(data.uniqueid);
+            return acc;
+        },[]);
+
+        // var sss = uniqueIds.length > 0 ? uniqueIds : [this.selectedOption.uniqueid]
+    
+        // this.clientS.gettopaddress(sss)
+        //     .subscribe(data => this.address = data)
+
+        this.optionsModal = true;
+    }
+    highlighted(data: any) {
+        this._highlighted = data;
     }
 
     ngAfterViewInit(): void {
@@ -280,5 +387,13 @@ export class RostersAdmin implements OnInit, OnDestroy, AfterViewInit {
 
     eventRightClick(data: any){
         console.log(data);
+        this.optionsModal=true;
+    }
+
+    handleCancel(){
+            this.optionsModal=false;
+    }
+    handleOk(){
+
     }
 }

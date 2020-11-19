17050d1eed25d0072babeb31d6b28f4d5f79755b
AhmadKamalMalik123 <ahmadkamal35201@gmail.com>
2020-11-19 18:33:24 +0500

    program coordinates

--- src/app/pages/admin/configuration/genrel-setup/programcoordinates.component.ts
+++ src/app/pages/admin/configuration/genrel-setup/programcoordinates.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { ListService } from '@services/index';
 import { GlobalService } from '@services/global.service';
 
 @Component({
@@ -10,6 +11,7 @@ import { GlobalService } from '@services/global.service';
 export class ProgramcoordinatesComponent implements OnInit {
 
   tableData: Array<any>;
+  staff:Array<any>;
   loading: boolean = false;
   modalOpen: boolean = false;
   current: number = 0;
@@ -19,17 +21,28 @@ export class ProgramcoordinatesComponent implements OnInit {
   heading:string = "Add New Program Coordinates"
   constructor(
     private globalS: GlobalService,
+    private listS:ListService,
     private cd: ChangeDetectorRef,
     private formBuilder: FormBuilder
   ){}
   
   ngOnInit(): void {
     this.buildForm();
-    this.tableData = [{ code:"Other",name:"K AGGARWAL"},{ code:"Other",name:"HILDA SMITH"}];
+    this.loadData();
+    // this.tableData = [{ code:"Other",name:"K AGGARWAL"},{ code:"Other",name:"HILDA SMITH"}];
     this.loading = false;
     this.cd.detectChanges();
   }
-  
+  loadData(){
+    let sql ="SELECT HACCCode as code , RecordNumber, Description as name FROM DataDomains WHERE Domain =  'CASE MANAGERS'";
+    this.loading = true;
+    this.listS.getlist(sql).subscribe(data => {
+      this.tableData = data;
+      console.log(this.tableData);
+      this.loading = false;
+    });
+    this.staff = ['DARLISION AWARD','DARLISION AWARD 2','DARLISION AWARD 3','DARLISION AWARD 3','DARLISION AWARD 4','DARLISION AWARD 5','DARLISION AWARD 6','DARLISION AWARD 7','ACT'];
+  }
   showAddModal() {
     this.resetModal();
     this.modalOpen = true;

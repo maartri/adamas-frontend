1bd79563386d54978e2f826b8962efe49ad17611
Mufeed Abbas <mufeed.ueo@gmail.com>
2021-03-18 02:42:15 +1100

    prod change

--- src/app/pages/admin/reports.ts
+++ src/app/pages/admin/reports.ts
@@ -887,12 +887,12 @@ export class ReportsAdmin implements OnInit, OnDestroy, AfterViewInit {
 
 
         
-        this.listS.GetAllPrograms().subscribe(x => this.programsArr = x);
-     /*   this.listS.getreportcriterialist({
+    //    this.listS.GetAllPrograms().subscribe(x => this.programsArr = x);
+        this.listS.getreportcriterialist({
             listType: 'PROGRAMS',
             includeInactive:false
         }).subscribe(x => this.programsArr = x);
-        */
+        
         this.listS.getreportcriterialist({
             listType: 'BRANCHES',
             includeInactive: false

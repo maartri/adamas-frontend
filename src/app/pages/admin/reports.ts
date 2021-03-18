ef3a10a13a0255c9267f707926a6e9d932bcde1b
AhmadKamalMalik123 <ahmadkamal35201@gmail.com>
2021-03-18 07:20:13 +0500

    changing

--- src/app/pages/admin/reports.ts
+++ src/app/pages/admin/reports.ts
@@ -374,7 +374,7 @@ export class ReportsAdmin implements OnInit, OnDestroy, AfterViewInit {
     chkbx_exclude_expirydates: boolean;
     chkbx_incl_Contacts: boolean;
     chkbx_asAddressLabel: boolean;
-    //chkbx_incl_additionalInfo: boolean;
+    chkbx_incl_additionalInfo: boolean;
     chkbx_incl_activeClients: boolean;
     chkbx_grpbyCoordinators: boolean;
     chkbx_incl_activeStaff: boolean;
@@ -991,7 +991,7 @@ export class ReportsAdmin implements OnInit, OnDestroy, AfterViewInit {
         this.chkbx_exclude_expirydates = false;
         this.chkbx_incl_Contacts = false;
         this.chkbx_asAddressLabel = false;
-    //    this.chkbx_incl_additionalInfo = false;
+       this.chkbx_incl_additionalInfo = false;
         this.chkbx_incl_activeClients = false;
         this.chkbx_include_enddated = false;
         this.chkbx_exclude_MandatoryChecking = false;

85c8543727542f952bf98e1d4430a38b85910c52
Mark <markaris.trinidad@gmail.com>
2021-09-23 21:43:34 +0800

    fixes

--- src/app/components/add-quote/add-quote.component.ts
+++ src/app/components/add-quote/add-quote.component.ts
@@ -538,28 +538,32 @@ export class AddQuoteComponent implements OnInit {
               return this.listS.getprogramlevel(x)
           }),
           switchMap(x => {                
-              this.IS_CDC = false;
-              if(x.isCDC){
-                  this.IS_CDC = true;
-                //   if(x.quantity && x.timeUnit == 'DAY'){
-                //       this.quoteForm.patchValue({
-                //           govtContrib: (x.quantity*365).toFixed(2),
-                //           programId: x.recordNumber
-                //       });
-                //       console.log(this.quoteForm.value)
-                //       this.remaining_fund = this.quoteForm.value.govtContrib;
-                //   }
-                //   this.detectChanges();
-                //   return this.listS.getpensionandfee();
-              }
+                this.IS_CDC = false;
+                if(x.isCDC){
+                    this.IS_CDC = true;
 
-                this.quoteForm.patchValue({
-                    govtContrib: x.quantity 
-                    ? (x.quantity * 365).toFixed(2) : 0,
-                    programId: x.recordNumber
-                });
+                    this.quoteForm.patchValue({
+                        govtContrib: x.quantity ? (x.quantity * 365).toFixed(2) : 0,
+                        programId: x.recordNumber
+                    });
+                    //   if(x.quantity && x.timeUnit == 'DAY'){
+                    //       this.quoteForm.patchValue({
+                    //           govtContrib: (x.quantity*365).toFixed(2),
+                    //           programId: x.recordNumber
+                    //       });
+                    //       console.log(this.quoteForm.value)
+                    //       this.remaining_fund = this.quoteForm.value.govtContrib;
+                    //   }
+                    //   this.detectChanges();
+                    //   return this.listS.getpensionandfee();
+                }
 
-                console.log(this.quoteForm.value)
+                // this.quoteForm.patchValue({
+                //     govtContrib: x.quantity ? (x.quantity * 365).toFixed(2) : 0,
+                //     programId: x.recordNumber
+                // });
+
+                // console.log(this.quoteForm.value)
                 // this.remaining_fund = this.quoteForm.value.govtContrib;
 
               this.detectChanges();

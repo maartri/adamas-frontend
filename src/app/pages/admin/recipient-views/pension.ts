51396587d857b26a10fc00d940b23416aa301047
AhmadKamalMalik123 <71849694+AhmadKamalMalik123@users.noreply.github.com>
2021-10-01 18:28:18 +0500

    added fix for pension

--- src/app/pages/admin/recipient-views/pension.ts
+++ src/app/pages/admin/recipient-views/pension.ts
@@ -239,6 +239,7 @@ export class RecipientPensionAdmin implements OnInit, OnDestroy {
             var address1 = this.pensionForm.value.address1;
             if(address1 == null || name == null){
                 this.globalS.wToast('Error', 'All manmdatpry fields must be completed'); 
+                this.isLoading = false;
                 return false;
             }
             

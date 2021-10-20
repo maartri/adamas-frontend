ab41ff2779339989f8debc94e8f543849e3dd88e
Ahmad <ahmadkamal35201@gmail.com>
2021-10-21 02:20:15 +0500

    updatedpritnisuue

--- src/app/pages/admin/configuration/recipients/debtor-terms.component.ts
+++ src/app/pages/admin/configuration/recipients/debtor-terms.component.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
-import { GlobalService, ListService, MenuService } from '@services/index';
+import { GlobalService, ListService, MenuService, PrintService } from '@services/index';
 import { SwitchService } from '@services/switch.service';
 import { NzModalService } from 'ng-zorro-antd';
 import { Subject } from 'rxjs';
@@ -42,6 +42,7 @@ whereString :string="WHERE ISNULL(DataDomains.DeletedRecord,0) = 0 AND (EndDate 
     private cd: ChangeDetectorRef,
     private switchS:SwitchService,
     private listS:ListService,
+    private printS:PrintService,
     private menuS:MenuService,
     private formBuilder: FormBuilder,
     private http: HttpClient,
@@ -234,15 +235,6 @@ whereString :string="WHERE ISNULL(DataDomains.DeletedRecord,0) = 0 AND (EndDate 
       
       var fQuery = "SELECT ROW_NUMBER() OVER(ORDER BY Description) AS Field1,Description as Field2 ,CONVERT(varchar, [enddate],105) as Field3 from DataDomains "+this.whereString+" Domain='DEBTORTERMS'";
       
-      const headerDict = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      }
-      
-      const requestOptions = {
-        headers: new HttpHeaders(headerDict)
-      };
-      
       const data = {
         "template": { "_id": "0RYYxAkMCftBE9jc" },
         "options": {
@@ -255,14 +247,12 @@ whereString :string="WHERE ISNULL(DataDomains.DeletedRecord,0) = 0 AND (EndDate 
           "head3" : "End Date",
         }
       }
-      this.http.post(this.rpthttp, JSON.stringify(data), { headers: requestOptions.headers, responseType: 'blob' })
-      .subscribe((blob: any) => {
+      this.printS.print(data).subscribe(blob => {  
         let _blob: Blob = blob;
         let fileURL = URL.createObjectURL(_blob);
         this.tryDoctype = this.sanitizer.bypassSecurityTrustResourceUrl(fileURL);
         this.loading = false;
-      }, err => {
-        console.log(err);
+        }, err => {
         this.loading = false;
         this.ModalS.error({
           nzTitle: 'TRACCS',
--- src/app/pages/admin/configuration/recipients/recipients-category.component.ts
+++ src/app/pages/admin/configuration/recipients/recipients-category.component.ts
@@ -10,6 +10,7 @@ import { MenuService } from '@services/menu.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
 import { NzModalService } from 'ng-zorro-antd';
+import { PrintService } from '@services/print.service';
 
 @Component({
   selector: 'app-recipients-category',
@@ -47,6 +48,7 @@ export class RecipientsCategoryComponent implements OnInit {
     private listS:ListService,
     private menuS:MenuService,
     private formBuilder: FormBuilder,
+    private printS:PrintService,
     private http: HttpClient,
     private fb: FormBuilder,
     private sanitizer: DomSanitizer,
@@ -235,15 +237,6 @@ export class RecipientsCategoryComponent implements OnInit {
       
       var fQuery = "SELECT ROW_NUMBER() OVER(ORDER BY Description) AS Field1,Description as Field2 ,CONVERT(varchar, [enddate],105) as Field3 from DataDomains "+this.whereString+" Domain='GROUPAGENCY'";
       
-      const headerDict = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      }
-      
-      const requestOptions = {
-        headers: new HttpHeaders(headerDict)
-      };
-      
       const data = {
         "template": { "_id": "0RYYxAkMCftBE9jc" },
         "options": {
@@ -256,14 +249,12 @@ export class RecipientsCategoryComponent implements OnInit {
           "head3" : "End Date",
         }
       }
-      this.http.post(this.rpthttp, JSON.stringify(data), { headers: requestOptions.headers, responseType: 'blob' })
-      .subscribe((blob: any) => {
+      this.printS.print(data).subscribe(blob => {  
         let _blob: Blob = blob;
         let fileURL = URL.createObjectURL(_blob);
         this.tryDoctype = this.sanitizer.bypassSecurityTrustResourceUrl(fileURL);
         this.loading = false;
-      }, err => {
-        console.log(err);
+        }, err => {
         this.loading = false;
         this.ModalS.error({
           nzTitle: 'TRACCS',

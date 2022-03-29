ef43eb53dea4686c41c2402076a5aff4e184454d
arshadblouch81 <arshadblouch81@gmail.com>
2022-03-30 09:56:04 +1100

    Arshad Fix

--- src/app/pages/admin/daymanager.ts
+++ src/app/pages/admin/daymanager.ts
@@ -774,6 +774,7 @@ pasteSelectedRecords(event:any){
     let dmType=event.selected.dmType;
     
     for ( let v of this._highlighted){  
+        this.selectedOption=v;
         v.date=moment(event.selected.date).format('YYYY/MM/DD');
         if (dmType=="1" || dmType=="2" || dmType=="11"){
          
@@ -1219,7 +1220,7 @@ ngAfterViewInit(){
             }
         }else if (operationDone=='Add'){
             if (this.viewType=='Staff'){
-                //this.current_roster = this.find_roster(parseInt(recordNo));
+                //let rst = this._highlighted.filter(x=>x.recordno==recordNo));
                 let clientCode =this.selectedOption.recipient;
                 let date= this.selectedOption.date
                 
@@ -1315,8 +1316,9 @@ ngAfterViewInit(){
             let res=data;       
             if (res.errorValue>0){
                 this.globalS.eToast('Error', res.errorValue +", "+ res.msg);
-                //if( Option=='Copy' ||Option=='Cut')
-                    //this.load_rosters();
+                
+                 if((Option=='Copy' || Option=='Cut') && (this._highlighted[this._highlighted.length-1].recordno==record.recordno))
+                     this.load_rosters();
                 return; 
             }
             
@@ -1727,7 +1729,8 @@ showConfirm(): void {
   }
 
   deleteRoster(){
-    for ( let v of this._highlighted){        
+    for ( let v of this._highlighted){  
+        this.selectedOption=v;      
         setTimeout(() => {
             this.ProcessRoster("Delete",v);                
         }, 100);   

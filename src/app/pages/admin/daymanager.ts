72fc9c60cde88e5653987e077091d9c136498ea2
arshadblouch81 <arshadblouch81@gmail.com>
2022-03-29 12:34:40 +1100

    Arshad Work

--- src/app/pages/admin/daymanager.ts
+++ src/app/pages/admin/daymanager.ts
@@ -1019,6 +1019,7 @@ load_rosters(){
     this.reload.next(true);
     
 }
+
 reLoadGridRosterModel(){
 
     this.info.IsMaster=false;
@@ -1066,6 +1067,7 @@ async openModal() {
   }
 ngOnInit(): void {
     this.token = this.globalS.decode(); 
+    console.log(this.token);
     this.buildForm(); 
     this.getLocalStorage();
     this.selectedPersonType ='Staff Management';
@@ -1246,13 +1248,22 @@ ngAfterViewInit(){
         return val;
       }
 
-      AddRoster(){
+      AddRoster(view:number){
         this.info.IsMaster=false;
         this.info.ViewType=this.viewType;
-        if (this.selectedOption!=null)        {
+        if (this.selectedOption!=null && view==1)        {
             this.info.StaffCode=this.selectedOption.staff;
             this.info.date=this.selectedOption.date;
-        }else  if (this.pastePosition!=null){
+            this.info.ViewType='Staff'
+        }else  if (this.selectedOption!=null && view==2) {        
+            this.info.StaffCode=this.selectedOption.recipient;
+            this.info.date=this.selectedOption.date;
+            this.info.ViewType='Recipient';
+        }else  if (this.selectedOption!=null && view==3) {        
+            this.info.StaffCode=this.token.code
+            this.info.date=this.selectedOption.date;
+            this.info.ViewType='Staff';
+        } else  if (this.pastePosition!=null){
             this.info.StaffCode=this.pastePosition.selected.carercode;
             this.info.date= moment(this.pastePosition.selected.date).format('YYYY/MM/DD');
         }else{
@@ -1508,9 +1519,9 @@ SaveNudgeUpDown(){
             
             sql.TableName='Roster ';          
             if (this.NudgeStatus=="Up")
-                sql.SetClause=`set Duration=Duration-${this.NudgeValue}/5`;
+                sql.SetClause=`set [start time]=left(dateAdd(minute,-${this.NudgeValue}, convert(time,[start time],111)),5)`;
             else
-                sql.SetClause=`set Duration=Duration+${this.NudgeValue}/5`;
+                sql.SetClause=`set [start time]=left(dateAdd(minute,${this.NudgeValue}, convert(time,[start time],111)),5)`;
 
            sql.WhereClause=` where RecordNo=${this.selectedOption.recordno} `;
        
